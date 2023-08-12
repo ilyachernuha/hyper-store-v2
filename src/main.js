5f6333efb1f84552eb513a2b864237c7ded2ec66
Illia Chernukha <ilyachernuha@gmail.com>
2023-08-12 21:22:29 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import "lazysizes";
+
+import Swiper from "swiper";
+import { Pagination, Navigation } from "swiper/modules";
+
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
+
+const swiper = new Swiper("#swiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 0,
+  // autoHeight: true,
+  modules: [Pagination],
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const reviews = new Swiper("#reviews-gallery", {
+  direction: "horizontal",
+  slidesPerView: 2,
+  spaceBetween: 10,
+  // autoHeight: true,
+  modules: [Navigation],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// const form = document.getElementById("form");
+
+// form.addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   try {
+//     const { data } = await axios.post("/api/", form, {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
