aac9c3d509c68cec474ab7f2ff20ddbc19b48b42
Illia Chernukha <ilyachernuha@gmail.com>
2023-08-15 17:18:20 +0300

    fix: gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://ilyachernuha.github.io",
-  base: "/hyper-store-v2",
-});
+export default defineConfig({});
